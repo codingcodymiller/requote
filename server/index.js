cbd89273dbb5639ee84be715cbebddaac7ab7c9d
Cody Miller <cody.miller@learningfuze.com>
2022-01-19 21:17:25 +0000

    added middleware to parse form encoded response

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ app.use(staticMiddleware);
 
 app.use(errorMiddleware);
 
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
@@ -19,5 +23,5 @@ app.post('/auth', (req, res) => {
   // const token = jwt.decode(req.body)
 
   // eslint-disable-next-line no-console
-  console.log('Parameters:', req.params, 'Headers:', req.headers);
+  console.log(req.body);
 });
