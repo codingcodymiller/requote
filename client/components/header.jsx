92b89a97d6d4b20d7aa9ec6b2d9f32b7ff54665a
Cody Miller <cody.miller@learningfuze.com>
2022-03-29 22:49:17 +0000

    Shifted background color to body element

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <header className="container-fluid bg-aqua-blue py-3">
+    <header className="container-fluid py-3">
       <div className="row">
         <img className="col-4 col-md-3 col-lg-2" src="/logo.svg" alt="ReQuote logo" />
       </div>
