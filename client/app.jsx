d8697509f0c887f226bcb53f4130107e86acda82
Cody Miller <cody.miller@learningfuze.com>
2022-04-03 21:08:40 -0700

    Created reusable section header component renamed SaveQuote to BookSearch, SaveQuote shuld be component for form to save quote

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from './components/header.jsx';
+import AppHeader from './components/app-header.jsx';
 // import Home from './pages/home';
-import SaveQuote from './pages/save-quote';
+import BookSearch from './pages/book-search';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
+        <AppHeader />
         <main className="bg-lavender-grey rounded-border-top main-content">
           <div className="container">
-            <SaveQuote />
+            <BookSearch />
           </div>
         </main>
       </>
---
+++ client/pages/book-search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SectionHeader from '../components/section-header';
+
+export default function BookSearch(props) {
+  return (
+    <>
+      <SectionHeader text="Save Quote" />
+    </>
+  );
+}
