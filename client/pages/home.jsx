ffd8d13b18d195dce98359c3b4578e04ee6e5041
Cody Miller <cody.miller@learningfuze.com>
2022-06-05 17:40:52 -0700

    Refresh error addressed, extraneous element removed

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
 
 export default function Home(props) {
   return (
     <>
       <a className="btn btn-primary" href="http://localhost:3000/api/login">Login</a>
-      <Navigate className="btn btn-primary" to="/library/book-search">Library</Navigate>
     </>
   );
 }
