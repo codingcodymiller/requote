3ef6b88ba435d328fb130d67b6ae16351c5c173d
Cody Miller <cody.miller@learningfuze.com>
2022-12-01 04:25:51 +0000

    Updated books table again to change synopsis to description

--- database/schema.sql
+++ database/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE "public"."books" (
 	"title" TEXT NOT NULL,
   "authors" TEXT[],
   "image" TEXT NOT NULL,
-  "synopsis" TEXT NOT NULL,
+  "description" TEXT NOT NULL,
 	"isbn" TEXT UNIQUE NOT NULL,
 	CONSTRAINT "books_pk" PRIMARY KEY ("bookId")
 ) WITH (
