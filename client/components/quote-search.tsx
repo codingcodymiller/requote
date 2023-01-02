c9bed3d1b192fdf9b09a5b8827aeeaa80273984e
Cody Miller <cody.miller@learningfuze.com>
2023-01-02 06:25:17 +0000

    Updated CSS to allow search bar to fill remaining area left over by icons

--- client/components/quote-search.tsx
+++ client/components/quote-search.tsx
@@ -14,20 +14,14 @@ type QuoteSearchProps = {
 export default function QuoteSearch (props: QuoteSearchProps) {
   const { disabled, sortType, isReversed, updateSortType, updateSearchTerm, updateIsReversed } = props;
   return (
-    <div className={`row justify-content-evenly align-items-center ${ disabled ? 'pe-none disabled' : '' }`}>
-      <div className="col-10 col-xs-11 pe-0">
-        <div className="row">
-          <SearchBar handleSearchSubmit={updateSearchTerm} placeholder="Search..." />
-        </div>
-      </div>
-      <div className="col-2 col-xs-1 px-1">
-        <QuoteSort
-          sortType={sortType}
-          isReversed={isReversed}
-          updateSortType={updateSortType}
-          updateIsReversed={updateIsReversed}
-        />
-      </div>
+    <div className={`d-flex flex-nowrap justify-content-evenly align-items-center ${ disabled ? 'pe-none disabled' : '' }`}>
+      <SearchBar className="flex-grow-1" handleSearchSubmit={updateSearchTerm} placeholder="Search..." />
+      <QuoteSort
+        sortType={sortType}
+        isReversed={isReversed}
+        updateSortType={updateSortType}
+        updateIsReversed={updateIsReversed}
+      />
     </div>
   )
 }
