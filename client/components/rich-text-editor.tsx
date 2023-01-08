c32d68d1018684a320073419671b6934b042b553
Cody Miller <cody.miller@learningfuze.com>
2023-01-08 23:58:55 +0000

    Swapped react-quill for TinyMCE. 0 npm vulnerabilities!

---
+++ client/components/rich-text-editor.tsx
@@ -0,0 +1,35 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+type RichTextEditorProps = {
+  id: string;
+  placeholder: string;
+  value: string;
+  onChange: Dispatch<SetStateAction<string>>;
+}
+
+export default function RichTextEditor({ id, placeholder, value, onChange }: RichTextEditorProps) {
+  return (
+    <Editor
+      apiKey='se3vqbded007gnz1j8m352wjdeyfscapoye2p195nmstwvb1'
+      id={id}
+      init={{
+        placeholder,
+        height: 500,
+        menubar: false,
+        plugins: [
+          'advlist', 'anchor', 'autolink', 'charmap',
+          'code', 'help', 'link', 'lists',
+          'searchreplace', 'visualblocks', 'wordcount'
+        ],
+        toolbar: 'h1 h2 | ' +
+          'bold italic strikethrough underline | alignleft aligncenter | ' +
+          'bullist numlist blockquote | link | ' +
+          'undo redo | help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+      }}
+      value={value}
+      onEditorChange={onChange}
+    />
+  );
+}
