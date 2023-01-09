013302e4f79ea2a08ab16613f0a9be7943d09b46
Cody Miller <cody.miller@learningfuze.com>
2023-01-09 05:55:03 +0000

    Adjusted BookCover component to be usable by both the LibraryBookList and BookCarousel components

--- client/components/book-cover.tsx
+++ client/components/book-cover.tsx
@@ -3,21 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import BookDetailsIcon from "./book-details-icon";
 
 export type BookData = {
-  id: number;
+  id?: number;
   image: string;
   title: string;
-  isbn: string;
+  isbn?: string;
+  className?: string;
+  details?: boolean;
+  rounded?: boolean;
 }
 
 export default function BookCover(props: BookData){
   const navigate = useNavigate();
   const showQuotes = () => {
-    navigate(`/quotes/${props.id}`, { replace: false });
+    navigate(`/quotes/${props.id || ''}`, { replace: false });
   }
   return (
-    <div className="col-6 col-sm-3 my-2 cursor-pointer hover-grow position-relative" onClick={showQuotes}>
-      <BookDetailsIcon isbn={props.isbn} className="position-absolute top-right-lg" />
-      <img src={props.image} alt={props.title} className="w-100 h-100 book-cover" />
+    <div className={`cursor-pointer hover-grow position-relative ${props.className || ''}`} onClick={showQuotes}>
+      {
+        props.details
+          ? <BookDetailsIcon isbn={props.isbn || ''} className="position-absolute top-right-lg" />
+          : <></>
+      }
+      <img src={props.image} alt={props.title} className={`w-100 h-100 book-cover ${props.rounded ? 'br-10' : ''}`} />
     </div>
   )
 }
