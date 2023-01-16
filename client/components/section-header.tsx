22adc92b9de234d4df7fa36e27ae3294df2bef1a
Cody Miller <cody.miller@learningfuze.com>
2023-01-16 05:17:45 +0000

    Increased size of section headers

--- client/components/section-header.tsx
+++ client/components/section-header.tsx
@@ -7,9 +7,9 @@ type HeaderProps = {
 export default function SectionHeader(props: HeaderProps) {
   return (
     <div className="row justify-content-center">
-      <h5 className="col-10 col-xl-12 p-2 mt-2 font-weight-light text-center text-light-grey section-header">
+      <h3 className="col-10 col-xl-12 p-2 mt-2 font-weight-light text-center text-light-grey section-header">
         {props.text}
-      </h5>
+      </h3>
     </div>
   );
 }
