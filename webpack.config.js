5f9a7973204b79735616cca03c44eab416cc5d64
Cody Miller <cody.miller@learningfuze.com>
2023-01-23 08:04:01 +0000

    Added one more safelist item for landing page transition

--- webpack.config.js
+++ webpack.config.js
@@ -114,7 +114,7 @@ module.exports = {
       chunkFilename: isDevelopment ? '[id].css' : '[id].[contenthash].css'
     }),
     new PurgeCSSPlugin({
-      safelist: [/^glider-/],
+      safelist: [/^glider-/, /^landing-/],
       paths: glob.sync([
         `${PATHS.client}/**/*`,
         `${PATHS.public}/**/*`
