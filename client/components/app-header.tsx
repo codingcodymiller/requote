9b75f53635ad87133694f019c03c620d547c7962
Cody Miller <cody.miller@learningfuze.com>
2023-01-25 05:23:25 +0000

    added Login/Logout button to nav bar

--- client/components/app-header.tsx
+++ client/components/app-header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCookie } from '../helpers';
 
 export default function AppHeader() {
   return (
@@ -10,19 +11,32 @@ export default function AppHeader() {
             <img className="logo" src="/images/logo.svg" alt="ReQuote logo" />
           </Link>
         </div>
-        <nav className="col-6 justify-content-end align-items-center d-none d-md-flex desktop-nav pe-4">
-          <Link to="/save-quote/book-search" className="text-dark-navy text-decoration-none px-2">
-            <i className="fa-light fa-pen-clip fa-xl m-1 mx-2"></i>
-            Save Quote
-          </Link>
-          <Link to="/library" className="text-dark-navy text-decoration-none px-2">
-            <i className="fa-light fa-books fa-xl m-1 mx-2"></i>
-            Library
-          </Link>
-          <Link to="/quotes" className="text-dark-navy text-decoration-none px-2">
-            <i className="fa-light fa-book-bookmark fa-xl m-1 mx-2"></i>
-            Quotes
-          </Link>
+        <nav className="col-6 d-flex justify-content-end align-items-center desktop-nav pe-4">
+          <div className="d-none d-md-block">
+            <Link to="/save-quote/book-search" className="text-dark-navy text-decoration-none px-2">
+              <i className="fa-light fa-pen-clip fa-xl m-1 mx-2"></i>
+              Save Quote
+            </Link>
+            <Link to="/library" className="text-dark-navy text-decoration-none px-2">
+              <i className="fa-light fa-books fa-xl m-1 mx-2"></i>
+              Library
+            </Link>
+            <Link to="/quotes" className="text-dark-navy text-decoration-none px-2">
+              <i className="fa-light fa-book-bookmark fa-xl m-1 mx-2"></i>
+              Quotes
+            </Link>
+          </div>
+          {
+            getCookie("user_token")
+            ? <a className="btn btn-navy align-middle d-flex align-items-center" href="/api/logout">
+                <i className="fa-regular fa-right-from-bracket pe-2"></i>
+                Logout
+              </a>
+            : <a className="btn btn-navy align-middle d-flex align-items-center" href="/api/login">
+                <i className="fa-brands fa-google pe-2"></i>
+                Login
+              </a>
+          }
         </nav>
         <nav className="col-12 d-flex justify-content-evenly align-items-center d-md-none mobile-nav">
           <Link to="/save-quote/book-search" className="text-center text-dark-navy text-decoration-none">
