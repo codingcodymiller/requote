e46c792202672fdd57400d28fa5e8a5f5696d6b3
Cody Miller <cody.miller@learningfuze.com>
2023-01-25 04:31:12 +0000

    Added edit icon to each QuoteListItem that redirects to edit page with quote id as a url parameter

--- client/components/quote-list-item.tsx
+++ client/components/quote-list-item.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export type QuoteData = {
-  page: number;
+  page: number | null;
   quoteText: string;
-  quoteId: number;
+  quoteId: string;
   bookTitle: string;
   bookAuthors: string[];
 }
@@ -14,16 +15,26 @@ type SingleQuoteProps = {
 
 export default function QuoteListItem(props: SingleQuoteProps){
   const {quote} = props;
+  const navigate = useNavigate();
   return (
     <div className="col-12 my-2 quote">
-      <div className="card h-100 shadow-sm p-3 position-relative">
+      <div className="card h-100 shadow-sm p-2 position-relative">
         <div className="row h-100">
           <div className="col-12">
-            <p
-              className={`${quote.page ? 'mt-3' : 'm-0'}`}
+            <div className="d-flex justify-content-between pb-1 px-1 mb-1 border-bottom">
+              <i
+                onClick={() => navigate(`/edit-quote/${quote.quoteId}`)}
+                className="fa-regular fa-pen-nib text-light-grey text-decoration-none cursor-pointer hover-aqua-blue"
+              ></i>
+              <p className="tiny-text m-0">
+                {quote.page ? `p.${quote.page}` : ""}
+              </p>
+            </div>
+            <div
+              className="px-1"
               dangerouslySetInnerHTML={{ __html: quote.quoteText}}
-            ></p>
-            <div className="row justify-content-end">
+            ></div>
+            <div className="row justify-content-end px-1">
               <div className="col-6 book-info">
                 <p className="m-0 text-end">
                   -{quote.bookAuthors[0]}
@@ -33,9 +44,6 @@ export default function QuoteListItem(props: SingleQuoteProps){
                 </p>
               </div>
             </div>
-            <p className="position-absolute top-right tiny-text">
-              {quote.page ? `p.${quote.page}` : ""}
-            </p>
           </div>
         </div>
       </div>
