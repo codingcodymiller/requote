4bccbb5a4eb60fef0071f29e4f4690fd4aedb08b
Cody Miller <cody.miller@learningfuze.com>
2023-01-28 04:50:31 +0000

    Added ability to return to previous search when viewing book details while searching for books

--- client/components/book-details-icon.tsx
+++ client/components/book-details-icon.tsx
@@ -1,5 +1,6 @@
-import React, { MouseEvent } from 'react';
+import React, { useContext, MouseEvent } from 'react';
 import { useNavigate } from 'react-router-dom'
+import { BookSearchContext, SearchContextValue } from './book-search'
 
 type BookDetailsIconProps = {
   isbn: string;
@@ -7,9 +8,14 @@ type BookDetailsIconProps = {
 }
 
 export default function BookDetailsIcon (props: BookDetailsIconProps) {
+  const searchContextData: SearchContextValue = useContext(BookSearchContext);
   const navigate = useNavigate();
   const showBookDetails = (event: MouseEvent) => {
     event.stopPropagation();
+    if(searchContextData && searchContextData.searchTerm && searchContextData.option){
+      sessionStorage.setItem("book-search-term", searchContextData.searchTerm);
+      sessionStorage.setItem("book-search-option", searchContextData.option.value)
+    }
     navigate(`/book-details/${props.isbn}`)
   }
 
--- client/components/book-search.tsx
+++ client/components/book-search.tsx
@@ -1,32 +1,64 @@
 import React, { useState } from 'react';
-import DropdownSearchBar, { Option } from './dropdown-search-bar';
+import DropdownSearchBar, { Option, OptionStore } from './dropdown-search-bar';
 import ResultList from './result-list';
 
+export const BookSearchContext = React.createContext<SearchContextValue>({} as SearchContextValue);
+
+export type SearchContextValue = {
+  searchTerm: string;
+  option: Option;
+}
+
 export default function BookSearch() {
-  const [results, updateResults] = useState([]);
-  const [searchTerm, updateSearchTerm] = useState('')
-  const searchBooks = (searchTerm: string, option: string) => {
-    updateSearchTerm(searchTerm)
-    fetch(`/api/search/${encodeURIComponent(`"${searchTerm}"`)}?type=${option}`)
-      .then(res => res.json())
-      .then(res => updateResults(res));
-  }
-  const options: Option[] = [
-    {
+  const options: OptionStore = {
+    title: {
       label: "Book",
       value: "title",
       placeholder: "Ex: The Hobbit"
     },
-    {
+    author: {
       label: "Author",
       value: "author",
       placeholder: "Ex: Stephen King"
     }
-  ]
+  }
+  const [results, updateResults] = useState([]);
+  const sessionSearchTerm = sessionStorage.getItem("book-search-term");
+  const sessionSearchOption = sessionStorage.getItem("book-search-option")
+  const [searchTerm, updateSearchTerm] = useState(sessionSearchTerm || "")
+  const [selectedOption, setSelectedOption] = useState(options[sessionSearchOption || "title"])
+  const searchBooks = (searchTerm: string, option: string) => {
+    fetch(`/api/search/${encodeURIComponent(`"${searchTerm}"`)}?type=${option}`)
+      .then(res => {
+        debugger;
+        return res.json()
+      })
+      .then(res => {
+        debugger;
+        updateResults(res)
+      });
+  }
+  if (sessionSearchTerm && sessionSearchOption){
+    searchBooks(sessionSearchTerm, sessionSearchOption)
+    sessionStorage.removeItem("book-search-term")
+    sessionStorage.removeItem("book-search-option")
+  }
+  const contextValue = {
+    searchTerm: searchTerm,
+    option: selectedOption
+  }
   return (
-    <>
-      <DropdownSearchBar label="Search by Book Title or Author" className="col-12" options={options} handleSearchSubmit={searchBooks} />
+    <BookSearchContext.Provider value={contextValue}>
+      <DropdownSearchBar
+        label="Search by Book Title or Author"
+        className="col-12"
+        options={options}
+        searchTerm={searchTerm}
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+        updateSearchTerm={updateSearchTerm}
+        handleSearchSubmit={searchBooks} />
       <ResultList results={results} searchTerm={searchTerm} />
-    </>
+    </BookSearchContext.Provider>
   );
 }
