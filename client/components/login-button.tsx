06fca7b336f1f502a3167240201182727030917b
Cody Miller <cody.miller@learningfuze.com>
2023-01-29 23:33:34 +0000

    Revised LoginButton component to only include the login button, with no logic to determine if the user is logged in.

--- client/components/login-button.tsx
+++ client/components/login-button.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { getCookie } from '../helpers';
 
-export default function LoginButton(){
-  const loggedIn = getCookie("access_token")
-  return loggedIn
-    ? <a className="btn btn-navy align-middle d-flex align-items-center mx-2" href="/api/logout">
-        <i className="fa-regular fa-right-from-bracket pe-2"></i>
-        Logout
-      </a>
-    : <a className="btn btn-navy align-middle d-flex align-items-center mx-2" href="/api/login">
-        <i className="fa-brands fa-google pe-2"></i>
-        Login
-      </a>
+export default function LoginButton() {
+  return (
+    <a className="btn btn-navy align-middle d-flex align-items-center mx-2" href="/api/login">
+      <i className="fa-brands fa-google pe-2"></i>
+      Login
+    </a>
+  );
 }
