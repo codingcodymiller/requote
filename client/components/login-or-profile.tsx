6b1872e126e5d6979f35325071aad990acb7f689
Cody Miller <cody.miller@learningfuze.com>
2023-01-30 19:54:04 +0000

    Shifted method of obtaining username from using getCookie helper to using context in LoginOrProfile

--- client/components/login-or-profile.tsx
+++ client/components/login-or-profile.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import LoginButton from './login-button';
 import UserProfileButton from './user-profile-button';
-import { getCookie } from '../helpers';
+import { UserContext } from '../app';
 
 export default function LoginOrProfile() {
-  const username = getCookie("username")
+  const { username } = useContext(UserContext);
   return username
     ? <UserProfileButton username={username} />
     : <LoginButton />
