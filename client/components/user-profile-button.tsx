0c43be22f691a6a6ca00d652c735a1261cc93313
Cody Miller <cody.miller@learningfuze.com>
2023-01-30 23:42:15 +0000

    Changed route for profile button to correct route to reach account settings page

--- client/components/user-profile-button.tsx
+++ client/components/user-profile-button.tsx
@@ -7,7 +7,7 @@ type ProfileButtonProps = {
 
 export default function UserProfileButton({ username }: ProfileButtonProps) {
   return (
-    <Link className="btn btn-navy align-middle d-flex align-items-center mx-2" to="/profile">
+    <Link className="btn btn-navy align-middle d-flex align-items-center mx-2" to="/account-settings">
       <i className={`fa-regular fa-lg fa-square-user pe-2`}></i>
       { username }
     </Link>
