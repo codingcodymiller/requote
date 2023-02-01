e99cfc069eff6bbb6240ecdde83d85708999d26b
Cody Miller <cody.miller@learningfuze.com>
2023-02-01 03:19:37 +0000

    Adjusted varchar length to 25 in database config

--- database/migrations/1-create-table-users.sql
+++ database/migrations/1-create-table-users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "public"."users" (
 	"userId" serial NOT NULL,
 	"token" TEXT UNIQUE NOT NULL,
-  "username" VARCHAR(20) UNIQUE NOT NULL,
+  "username" VARCHAR(25) UNIQUE NOT NULL,
 	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
 );
