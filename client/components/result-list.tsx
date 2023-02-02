cb5e6eaf4c93e4643f95df4cd0fa459b01ccb355
Cody Miller <cody.miller@learningfuze.com>
2023-02-02 23:51:54 +0000

    Only display NoResults component if a value has already been submitted for search, not just typed into the input

--- client/components/result-list.tsx
+++ client/components/result-list.tsx
@@ -6,10 +6,11 @@ import NoResults from './no-results';
 type ResultsListProps = {
   results: BookData[];
   searchTerm: string;
+  formSubmitted: boolean;
 }
 
 export default function ResultList(props: ResultsListProps) {
-  if (!props.results.length && props.searchTerm) return <NoResults />;
+  if (!props.results.length && props.searchTerm && props.formSubmitted) return <NoResults />;
   const results = props.results
     .map((book: BookData) => <BookListItem book={book} key={book.isbn} />);
 
