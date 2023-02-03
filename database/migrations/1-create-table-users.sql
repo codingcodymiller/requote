605a11f2187c070f611a6bcba976253093e03ebb
Cody Miller <cody.miller@learningfuze.com>
2023-02-03 00:08:43 +0000

    Adjsted users table to include column for privacy preference in preparation for future feature to prefer private or public for quotes on a user level

--- database/migrations/1-create-table-users.sql
+++ database/migrations/1-create-table-users.sql
@@ -2,5 +2,6 @@ CREATE TABLE "public"."users" (
 	"userId" serial NOT NULL,
 	"token" TEXT UNIQUE NOT NULL,
   "username" VARCHAR(25) UNIQUE NOT NULL,
+  "prefersPrivate" BOOLEAN DEFAULT FALSE,
 	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
 );
