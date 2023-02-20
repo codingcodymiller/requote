88007056bd2fb416579793310e462cb81fe19542
Cody Miller <cody.miller.primary@gmail.com>
2023-02-20 16:13:56 +0000

    Testing GitHub Action

--- jest.config.js
+++ jest.config.js
@@ -22,5 +22,7 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 
   // Module file extensions for importing
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  reporters: ['default', 'jest-junit']
 };
