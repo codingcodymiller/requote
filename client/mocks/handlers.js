2969288300456851138db85e965913025e93957a
Cody Miller <cody.miller.primary@gmail.com>
2023-02-22 00:40:37 +0000

    Created file for Mock Service Worker server config and handlers to be mocked

---
+++ client/mocks/handlers.js
@@ -0,0 +1,26 @@
+import { rest } from 'msw';
+export const handlers = [
+  rest.all('/api/books', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.set('Content-Type', 'application/json'),
+      ctx.json([
+        {
+          id: 'test1234',
+          title: 'Test Book',
+          image: '/images/placeholder-image.jpg',
+          authors: ['codingcodymiller'],
+          isbn: '1231-123-12313',
+          description: 'Don\'t be alarmed, this is just a test'
+        },
+        {
+          id: 'test12345',
+          title: 'Test Book: The Sequel',
+          image: '/images/placeholder-image.jpg',
+          authors: ['codingcodymiller'],
+          isbn: '1231-123-12314',
+          description: 'This isn\'t your grandpa\'s test.'
+        }
+      ]));
+  })
+];
