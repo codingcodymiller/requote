f1e97da78f0c7fa232a1a6a2b3dab3f9dc008fe5
Cody Miller <cody.miller.primary@gmail.com>
2023-02-23 21:41:59 +0000

    Set up error event handler for a broken image to show the placeholder image

--- client/components/book-cover.tsx
+++ client/components/book-cover.tsx
@@ -12,8 +12,9 @@ export type BookCoverProps = {
 }
 
 export default function BookCover(props: BookCoverProps){
-  const { details, className, rounded, callback } = props;
-  const { isbn, title, image } = props.book;
+  const { book, details, className, rounded, callback } = props;
+  const { isbn, title, image } = book;
+  const [imagePath, updateImagePath] = useState(image)
   const [isLoading, setLoadState] = useState(true);
   return (
     <div role="button" className={`cursor-pointer hover-grow position-relative ${className || ''}`} onClick={callback ? () => callback(props.book) : undefined}>
@@ -22,7 +23,7 @@ export default function BookCover(props: BookCoverProps){
           ? <BookDetailsIcon isbn={isbn} className="position-absolute top-right-lg fs-5 hover-grow-lg cancel-ancestor-hover-grow" />
           : <></>
       }
-      <img src={image} alt={title} onLoad={() => setLoadState(false)} className={`w-100 h-100 book-cover ${rounded ? 'br-10' : ''}  ${isLoading ? 'd-none' : ''}`} />
+      <img src={imagePath} alt={title} onLoad={() => setLoadState(false)} onError={() => updateImagePath('/images/placeholder-image.jpg')} className={`w-100 h-100 book-cover ${rounded ? 'br-10' : ''}  ${isLoading ? 'd-none' : ''}`} />
       {
         isLoading
           ? <div className={`w-100 h-100 pt-3`}>
