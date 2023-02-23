8856008abb78492b00547d400f8ae54b639b080d
Cody Miller <cody.miller.primary@gmail.com>
2023-02-23 22:49:00 +0000

    Added tests to ensure that a details icon is shown if the details prop is passed to the component, and a test to ensure that a placeholder image is shown for any broken images

--- client/__tests__/book-cover.test.tsx
+++ client/__tests__/book-cover.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
-import { fireEvent, render, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { rest } from 'msw';
-import { server } from '../mocks/server'
 
 import BookCover from '../components/book-cover'
 
@@ -18,12 +16,32 @@ const book = {
 describe('<BookCover />', () => {
   it('Should have a loading element present while the book cover image is loading.', () => {
     render(<BookCover book={book} />, { wrapper: BrowserRouter })
+
     expect(screen.getByRole('status')).toBeInTheDocument();
   })
   it('Should remove the loading spinner once the book\'s cover image is loaded.', async () => {
     render(<BookCover book={book} />, { wrapper: BrowserRouter })
+
     const bookCover = screen.getByRole('img')
     fireEvent.load(bookCover)
+
     expect(screen.queryByRole('status')).toBeNull();
+  })
+  it('Should have a details icon if a details prop is passed to the component.', async () => {
+    render(<BookCover book={book} details={true} />, { wrapper: BrowserRouter })
+
+    const bookCover = screen.getByRole('img')
+    fireEvent.load(bookCover)
+
+    expect(screen.getByTitle(/book details/i)).toBeInTheDocument();
+  })
+  it('Should show the placeholder image if the book cover url is invalid.', async () => {
+    render(<BookCover book={book} details={true} />, { wrapper: BrowserRouter })
+
+    const bookCover: HTMLImageElement = screen.getByRole('img')
+    fireEvent.error(bookCover)
+    fireEvent.load(bookCover)
+
+    expect(bookCover.src).toContain('placeholder');
   })
 })
