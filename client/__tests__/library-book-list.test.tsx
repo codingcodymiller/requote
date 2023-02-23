0a9b4197f9ab285e496d6048b099d84db85665be
Cody Miller <cody.miller.primary@gmail.com>
2023-02-23 03:47:41 +0000

    Included a test to ensure that the loading spinner is shown when the LibraryBookList component first loads, as well as a test to make sure that a message saying that no books are saved is shown when a user has no books saved yet.

--- client/__tests__/library-book-list.test.tsx
+++ client/__tests__/library-book-list.test.tsx
@@ -2,16 +2,36 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { rest } from 'msw';
+import { server } from '../mocks/server'
 
 import LibraryBookList from '../components/library-book-list'
 
 describe('<LibraryBookList />', () => {
-
-  test('Should have a list of books that the user has saved quotes for', async () => {
+  it('Should have a loading element present while data is retrieved', () => {
     render(<LibraryBookList />, { wrapper: BrowserRouter })
-    await waitForElementToBeRemoved(screen.getByText("Loading..."))
+    expect(screen.getByText((/loading/i))).toBeInTheDocument();
+  })
+  it('Should load a list of books that the user has saved quotes for', async () => {
+    render(<LibraryBookList />, { wrapper: BrowserRouter })
+    await waitForElementToBeRemoved(screen.getByText(/loading/i))
 
-    const books = screen.getAllByAltText(/Test Book/i)
+    const books = screen.getAllByRole('button')
     expect(books).toHaveLength(2);
+  })
+  it('Should show that there are no books when a user has not saved any quotes for any books yet.', async () => {
+    server.use(
+      rest.get('/api/books', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set('Content-Type', 'application/json'),
+          ctx.json([])
+        );
+      })
+    )
+    render(<LibraryBookList />, { wrapper: BrowserRouter })
+    await waitForElementToBeRemoved(screen.getByText(/loading/i))
+
+    expect(screen.getByText((/no books/i))).toBeInTheDocument();
   })
 })
