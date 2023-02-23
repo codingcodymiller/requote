af7ff0793a9c2e89d1fb344d24978cbf2eb13d96
Cody Miller <cody.miller.primary@gmail.com>
2023-02-23 21:41:07 +0000

    Revised method of finding loading spinner to be by role rather than by the hidden text

--- client/__tests__/library-book-list.test.tsx
+++ client/__tests__/library-book-list.test.tsx
@@ -10,11 +10,11 @@ import LibraryBookList from '../components/library-book-list'
 describe('<LibraryBookList />', () => {
   it('Should have a loading element present while data is retrieved', () => {
     render(<LibraryBookList />, { wrapper: BrowserRouter })
-    expect(screen.getByText((/loading/i))).toBeInTheDocument();
+    expect(screen.getByRole('status')).toBeInTheDocument();
   })
   it('Should load a list of books that the user has saved quotes for', async () => {
     render(<LibraryBookList />, { wrapper: BrowserRouter })
-    await waitForElementToBeRemoved(screen.getByText(/loading/i))
+    await waitForElementToBeRemoved(screen.getByRole('status'))
 
     const books = screen.getAllByRole('button')
     expect(books).toHaveLength(2);
@@ -30,7 +30,7 @@ describe('<LibraryBookList />', () => {
       })
     )
     render(<LibraryBookList />, { wrapper: BrowserRouter })
-    await waitForElementToBeRemoved(screen.getByText(/loading/i))
+    await waitForElementToBeRemoved(screen.getByRole('status'))
 
     expect(screen.getByText((/no books/i))).toBeInTheDocument();
   })
