2371ee9838b67d794afdd64b6aa46640fe928013
Cody Miller <cody.miller.primary@gmail.com>
2023-02-27 18:19:47 +0000

    Added test for QuoteControls to ensure that filter, reverse, and share icons are visible on personal feed

---
+++ client/__tests__/quote-controls.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom'
+
+import QuoteControls from '../components/quote-controls'
+
+describe('<QuoteControls />', () => {
+  it('Should show icons to filter, reverse, and share quotes if viewing a personal feed.', () => {
+    Object.defineProperty(window.document, 'cookie', {
+      writable: true,
+      value: 'username=codingcodymiller',
+    });
+    render(
+      <MemoryRouter initialEntries={['/quotes']}>
+        <QuoteControls sortType='date' isReversed={false} updateIsReversed={() => {}} updateSortType={() => {}} />
+      </MemoryRouter>
+    )
+    expect(screen.getByTitle('filter')).toBeVisible();
+    expect(screen.getByTitle('reverse')).toBeVisible();
+    expect(screen.getByTitle('share')).toBeVisible();
+  })
+})
