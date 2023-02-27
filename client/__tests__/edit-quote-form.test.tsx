613bad59055a8f3b2a9a8a7633b0571df57729fc
Cody Miller <cody.miller.primary@gmail.com>
2023-02-27 04:02:14 +0000

    Created test to ensure submit button is disabled when there is no text in the quote field

--- client/__tests__/edit-quote-form.test.tsx
+++ client/__tests__/edit-quote-form.test.tsx
@@ -5,6 +5,8 @@ import { waitFor } from '@testing-library/dom'
 import '@testing-library/jest-dom'
 
 import EditQuoteForm from '../components/edit-quote-form'
+import { server } from '../__mocks__/server';
+import { rest } from 'msw';
 
 describe('<EditQuoteForm />', () => {
   it('Should have inputs automatically filled with the present values for the quote.', () => {
@@ -18,5 +20,25 @@ describe('<EditQuoteForm />', () => {
     fireEvent.click(infoIcon);
     const tooltip = screen.getByLabelText('Public quotes will be viewable by other users when shared', { exact: false });
     expect(tooltip).toHaveAttribute('data-balloon-visible', 'true')
+  })
+  it('Should have tooltip visible when the info icon for making a quote public is clicked.', () => {
+    server.use(
+      rest.get('/api/quote/:quoteId', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set('Content-Type', 'application/json'),
+          ctx.json({
+            page: 1,
+            quoteText: '',
+            isPrivate: false
+          }));
+      })
+    )
+    render(<EditQuoteForm quoteId="1" />, { wrapper: BrowserRouter })
+    const page = screen.getByRole('spinbutton')
+    waitFor(() => expect(page).toHaveValue('1'));
+
+    const submit = screen.getByTitle(/submit/i);
+    expect(submit).toBeDisabled()
   })
 })
