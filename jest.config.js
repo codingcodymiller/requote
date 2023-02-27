2bace90a0db2010f3286851e1577f014c4b84262
Cody Miller <cody.miller.primary@gmail.com>
2023-02-27 20:11:46 +0000

    Added scss files to the list of files for jest to mock

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/client/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/client/__mocks__/styleMock.js'
+    '\\.(css|less|scss)$': '<rootDir>/client/__mocks__/styleMock.js'
   },
   reporters: [
     'default',
