7a8263c2fbc4822de77bef987cc4d42e2fd54898
Cody Miller <cody.miller.primary@gmail.com>
2023-02-27 03:56:58 +0000

    Added test to check that tooltip is visible when the info icon is clicked

--- client/__tests__/edit-quote-form.test.tsx
+++ client/__tests__/edit-quote-form.test.tsx
@@ -12,4 +12,11 @@ describe('<EditQuoteForm />', () => {
     const page = screen.getByRole('spinbutton')
     waitFor(() => expect(page).toHaveValue('1'));
   })
+  it('Should have tooltip visible when the info icon for making a quote public is clicked.', () => {
+    render(<EditQuoteForm quoteId="1" />, { wrapper: BrowserRouter })
+    const infoIcon = screen.getByTitle(/public explanation/i)
+    fireEvent.click(infoIcon);
+    const tooltip = screen.getByLabelText('Public quotes will be viewable by other users when shared', { exact: false });
+    expect(tooltip).toHaveAttribute('data-balloon-visible', 'true')
+  })
 })
